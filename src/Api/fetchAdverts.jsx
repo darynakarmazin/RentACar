75a4e55e0cd7e8718c127a693bdd6e79610b56db
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-10-06 19:29:07 +0300

    Updated fn for pagination

--- src/Api/fetchAdverts.jsx
+++ src/Api/fetchAdverts.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://651fc0b1906e276284c373de.mockapi.io/api/v1/";
 const limit = 8;
-  
+
 async function fetchAdverts(page) {
   try {
-    const res = await axios.get(`/adverts?&limit=${limit}&page=${page}`);
+    const res = await axios.get(`/adverts/?limit=${limit}&page=${page}`);
     return res.data;
   } catch (error) {
     console.error(error);
