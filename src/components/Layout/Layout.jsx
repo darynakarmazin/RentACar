52d9c2327a263e8da47395bc0da459d8c5ad75e8
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-10-06 11:52:42 +0300

    Created Layout (Header) with main navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import LogoImg from "./../../img/car-icon.svg";
 
 function Layout() {
   return (
     <div>
-      <div> Layout </div>
+      <div>
+        <NavLink to="/">
+          <img src={LogoImg} alt="Car logo" width={40} /> RentACar
+        </NavLink>
+        <div>
+          <NavLink to="/catalog">Catalog</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
