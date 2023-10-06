7fd0824f2c7fa97a44f98f6402bd010d1645ea86
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-10-06 12:27:48 +0300

    Added basic MainPage maukup

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,27 @@
+import { HeroContainer, HeroContent } from "./Main.styled";
+
 function Main() {
-  return <h1>Main Page</h1>;
+  return (
+    <div>
+      <div>
+        <HeroContainer>
+          <HeroContent>
+            <h1>RentACar</h1>
+            <p>
+              RentACar is a user-friendly application designed for companies
+              offering car rental services in Ukraine, providing seamless access
+              to a diverse fleet of vehicles.
+            </p>
+            <p>
+              Discover RentACar, your gateway to convenient car rental services
+              in Ukraine. Streamline your vehicle selection process with our
+              intuitive application.
+            </p>
+          </HeroContent>
+        </HeroContainer>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
---
+++ src/pages/Main/Main.styled.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import bgImg from "./../../img/car-bgc-2.jpg";
+
+export const HeroContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row-reverse;
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0.3),
+      rgba(255, 255, 255, 0.1)
+    ),
+    url(${bgImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const HeroContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  padding: 60px 24px 60px 0;
+`;
