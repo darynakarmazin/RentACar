87173799b2c23298249f0d712abbaa725e1e85b4
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-10-08 17:29:25 +0300

    Updated project variables;

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/RentACar">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter basename="/RentACar">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
   // </React.StrictMode>
 );
